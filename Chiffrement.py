0fa15c082bfcea6e644bda1cc66d273cdf188257
NGabriela <gabriela.nnitulescu@yahoo.com>
2014-03-06 19:12:12 +0200

    Create Chiffrement.py

---
+++ Chiffrement.py
@@ -0,0 +1,17 @@
+def decaler(caractere, decalage) :
+    if caractere.islower():
+      return chr(((ord(caractere)-97+decalage) % 26)+97)
+    elif caractere.isupper():
+      return chr(((ord(caractere)-65+decalage) % 26 )+65)
+    else:
+      return caractere # non alphabétique
+      
+def cesar(phrase, shift):
+     message = ""
+     for caractere in phrase:
+      message += decaler(caractere, shift)
+      return message
+      
+texte_clair = "Alea jacta est!"
+texte_code = cesar(texte_clair, 3)
+print texte_clair, "devient", texte_code
