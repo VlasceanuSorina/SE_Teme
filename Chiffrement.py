2ceb601ff68c7b65f5eff24b392f08a74cdd4312
NGabriela <gabriela.nnitulescu@yahoo.com>
2014-03-07 09:28:07 +0200

    Update Chiffrement.py

--- Chiffrement.py
+++ Chiffrement.py
@@ -1,17 +1,37 @@
-def decaler(caractere, decalage) :
-    if caractere.islower():
-      return chr(((ord(caractere)-97+decalage) % 26)+97)
-    elif caractere.isupper():
-      return chr(((ord(caractere)-65+decalage) % 26 )+65)
-    else:
-      return caractere # non alphabétique
-      
-def cesar(phrase, shift):
-     message = ""
-     for caractere in phrase:
-      message += decaler(caractere, shift)
-      return message
-      
-texte_clair = "Alea jacta est!"
-texte_code = cesar(texte_clair, 3)
-print texte_clair, "devient", texte_code
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+     
+abc = 'abcdefghijklmnopqrstuvwxyz'
+     
+def encrypt(message, key):
+     
+    text_encrypted = ''
+     
+    for leter in message:
+	    sum = abc.find(leter) + key
+	    modulo = int(sum) % len(abc)
+	    text_encrypted = text_encrypted + str(abc[modulo])
+     
+    return text_encrypted
+     
+def decrypt(message, key):
+     
+    text_encrypted = ''
+     
+    for leter in message:
+	    sum = abc.find(leter) - key
+	    modulo = int(sum) % len(abc)
+	    text_encrypted = text_encrypted + str(abc[modulo])
+     
+    return text_encrypted
+     
+def main():
+    ms = str(raw_input('message a encrypt: ')).lower()
+    n = int(raw_input('key numerica: '))
+    print encrypt(ms,n)
+    cms = str(raw_input('message a decrypt: ')).lower()
+    cn = int(raw_input('key numerica: '))
+    print decrypt(cms,cn)	
+     
+if __name__ == '__main__':
+    main()
